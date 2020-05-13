87f66415f9ce76806b54ad445793f9f7546147dc
Chris Till <cpaultill@gmail.com>
2020-05-13 04:07:57 -0400

    initial commit lesson 1

---
+++ src/homepage.component.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+const Homepage = () => {
+    return (
+        <div className="homepage">
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1>
+                            HATS
+                        </h1>
+                        <span>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1>
+                            JACKETS
+                        </h1>
+                        <span>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1>
+                            SNEAKERS
+                        </h1>
+                        <span>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1>
+                            WOMENS
+                        </h1>
+                        <span>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1>
+                            MENS
+                        </h1>
+                        <span>SHOP NOW</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Homepage;
